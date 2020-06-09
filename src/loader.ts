fc215a1de6cd8f9b454cc36462860661608cc797
Yanis HEDJEM <yanishproduction@gmail.com>
2020-06-09 15:24:32 +0200

    fix: application/wasm forced

--- src/loader.ts
+++ src/loader.ts
@@ -12,7 +12,7 @@ if (!WebAssembly.instantiateStreaming) {
 
 export const load: (path: string) => Promise<void> = async (path) => {
   try {
-    const {instance} = await WebAssembly.instantiateStreaming(fetch(path), go.importObject);
+    const {instance} = await WebAssembly.instantiateStreaming(fetch(path).then(r => {r.headers.set("Content-Type", "application/wasm"); return r}), go.importObject);
 
     await go.run(instance);
   } catch (e) {
