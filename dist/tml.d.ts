a558db2e276498259be18efb2244002ce9281497
Yanis HEDJEM <yanishproduction@gmail.com>
2020-06-09 15:09:46 +0200

    build: add dist

--- dist/tml.d.ts
+++ dist/tml.d.ts
@@ -1,3 +1,4 @@
 import { ParserConfig, Workout } from "./types";
+export declare let onReady: () => void;
 export declare function parse(input: string, conf?: ParserConfig): Workout;
 export declare function withDefaultConfig(): ParserConfig;
